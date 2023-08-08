080f83442c35c0868b7dee6a3e3e7679f95be5a7
Frank Mastronardi <mastronardif@gmail.com>
2023-08-07 22:14:09 -0400

    cleanup

--- tutorial-web/src/main/web/src/app/app.module.ts
+++ tutorial-web/src/main/web/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
+// const appRoutes: Routes = [
+//   // { path: '', component: HomeComponent },
+//   // { path: '**', component: PageNotFoundComponent },
+// ];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule
+    //RouterModule.forRoot(Routes, { useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
